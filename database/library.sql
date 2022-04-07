0a34d8847ca556a37e6c08323d2be52e16b31b72
k-rishabh <krishabh2002@gmail.com>
2022-04-07 14:49:49 +0530

    modified library.sql

--- database/library.sql
+++ database/library.sql
@@ -5,7 +5,15 @@ create table books(
     bookID int PRIMARY KEY AUTO_INCREMENT,
     title varchar(50),
     author varchar(50),
-    publisher varchar(50)
+    publisher varchar(50),
+    genre varchar(20)
+);
+
+create table locations(
+    hall int,
+    shelf int,
+    genre varchar(20) REFERENCES books(genre) NOT NULL,
+    PRIMARY KEY(hall, shelf)
 );
 
 create table acType(
@@ -51,6 +59,16 @@ create table reservations(
 
 insert into books values(1, 'hary poter', 'jkr', 'penguin');
 
-insert into acType values(1,'Diamond',1000,100,30,10);
-insert into acType values(2,'Gold',800,80,20,20);
-insert into acType values(3,'Silver',600,60,10,30);
+insert into acType values(1, 'Platinum', 2000, 5, 4, 50);
+insert into acType values(2, 'Gold', 1800, 4, 3, 50);
+insert into acType values(3, 'Silver', 1500, 2, 2, 40);
+insert into acType values(4, 'Bronze', 1200, 1, 1, 30);
+
+insert into timings values(1, 8:00, 8:50);
+insert into timings values(2, 9:00, 9:50);
+insert into timings values(3, 10:00, 10:50);
+insert into timings values(4, 11:00, 11:50);
+insert into timings values(5, 14:00, 14:50);
+insert into timings values(6, 15:00, 15:50);
+insert into timings values(7, 16:00, 16:50);
+insert into timings values(8, 17:00, 17:50);
